63d9fe2f27f3342d8c72b671ddbc6492231b9898
zaki031 <115791170+zaki031@users.noreply.github.com>
2023-04-13 23:10:35 +0200

    even shorter

--- script.py
+++ script.py
@@ -1,2 +1,1 @@
-list_=open("txt.txt").read()
-print(''.join(chr(len(i)) for i in list_.replace("M", "").split("C")))
+print(''.join(chr(len(i)) for i in open("txt.txt").read().replace("M", "").split("C")))
