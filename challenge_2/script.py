abc63c467160e7068be2781bb9c325ee44e5d810
zaki031 <115791170+zaki031@users.noreply.github.com>
2023-04-16 03:44:40 +0200

    edit

--- challenge_2/script.py
+++ challenge_2/script.py
@@ -1,16 +1,16 @@
 r,n={'I':1,'V':5,'X':10,'L':50,'C':100,'M':1000},{1000:'M',100:'C'}
 def c(i):
-    d,p = 0,0
+    d,p=0,0
     for n in i[::-1]:
         v=r[n]
-        d+=v
-        p = v
+        if v<p:d-=v
+        else:d += v
+        p=v
     return d
 h,k=1100,''
 for v in n.keys():
-    while h>=v:
-        k+= n[v]
-        h-= v
-print(k,'since',''.join(str(c(i))+'/'for i in "V/III/MCMLXXXV".split("/")))
+    k+=n[v]
+    h-=v
+print(k,'since',''.join(str(c(i))+'/'for i in "V/III/MCMLXXXV".split("/"))[:-1])
 
 
