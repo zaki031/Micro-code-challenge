98d66cb7e8f083a33d705e6f0ff56f7b3c3b51e4
zaki031 <115791170+zaki031@users.noreply.github.com>
2023-04-17 00:33:11 +0200

    comments

--- challenge_2/script.py
+++ challenge_2/script.py
@@ -16,9 +16,9 @@ roman_map = {
 }
 
 def num_to_roman(num):
-    #Convert an integer to Roman numbers
+    # Convert an integer to Roman numbers
     roman_num = ''
-    #looping through the roman_numbers dictionary items
+    # Looping through the roman_numbers dictionary items
     for roman, value in roman_map.items():
         while num >= value:
             roman_num += roman
@@ -26,37 +26,37 @@ def num_to_roman(num):
     return roman_num
 
 def roman_to_num(roman_numeral):
-    #Convert Roman numbers to an integer
+    # Convert Roman numbers to an integer
     num = 0
     previous_value = 0
-    #looping through the reversed roman_numbers dictionary
+    # Looping through the reversed roman_numbers dictionary
     for roman in reversed(roman_numeral):
         
         # If the value of the current Roman numeral is less than the previous value,
-        # subtract it from the total sum to account for cases like IV (4) or IX (9)
+        # Subtract it from the total sum to account for cases like IV (4) or IX (9)
         value = roman_map[roman]
         if value < previous_value:
             num -= value
         # In other cases add it to the total sum
         else:
             num += value
-        #update the prev value for next iteration
+        # Update the prev value for next iteration
         previous_value = value
     return num
 
-#spliting the input into a num part and a roman_num part
+# Spliting the input into a num part and a roman_num part
 
 original_value = input().split(" since ")
 
-#num part
+# Num part
 num = int(original_value[0])
-#coverting the number to it's roman value
+# Coverting the number to it's roman value
 roman = num_to_roman(num)
 
 
 
 
-#roman_num part, splitting it into a DD/MM/YYYY format
+# Roman_num part, splitting it into a DD/MM/YYYY format
 rom = original_value[1].split('/')
 
 
@@ -64,7 +64,7 @@ rom = original_value[1].split('/')
 
 
 
-#Transfering the roman typed date into a normal date
+# Transfering the roman typed date into a normal date
 date = ''
 for i in rom:
     date+=str(roman_to_num(i))+'/'
@@ -72,5 +72,5 @@ for i in rom:
 
 
 
-#printing the num_to_roman value + the roman_to_num value
+# Printing the num_to_roman value + the roman_to_num value
 print(roman, 'since', date[:-1])
